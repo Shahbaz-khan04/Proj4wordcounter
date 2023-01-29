917b01da1650a7d9cb1a680b24b418e37d05dd89
shahbaz <kingshahbaz0313@gmail.com>
2023-01-29 22:19:46 +0500

    Completed-Project

---
+++ index.ts
@@ -0,0 +1,61 @@
+#!usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+};
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Welcome to the word count cli tool"
+  );
+  await sleep();
+  rainbowTitle.stop();
+}
+welcome();
+
+async function askQuestions() {
+  var count: number = 0;
+  let questions = await inquirer.prompt([
+    {
+      name: "ques",
+      type: "input",
+      message: chalk.rgb(
+        187,
+        143,
+        206
+      )("Please enter the paragraph you want to convert: "),
+    },
+  ]);
+  const arr = questions.ques.split(" ");
+  console.log(`Words in paragraph: ${arr.length}`);
+  for (let i = 0; i < arr.length; i++) {
+    count += arr[i].length;
+  }
+  console.log(count);
+}
+// askQuestions();
+
+async function startAgain() {
+  do {
+    await askQuestions();
+    var again = await inquirer.prompt([
+      {
+        name: "restart",
+        type: "input",
+        message: chalk.rgb(
+          187,
+          143,
+          206
+        )("Do yo want to restart? Press Y for yes."),
+      },
+    ]);
+  } while (again.restart == "y" || again.restart == "Y");
+}
+
+startAgain();
